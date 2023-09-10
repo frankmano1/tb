dfe2ad6632264e63c5a32020c9e9cf8ea6dc4c78
frankmano1 <109146593+frankmano1@users.noreply.github.com>
2023-09-11 00:39:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// config.js
-
 module.exports = {
-    telegramBotToken: '6014587457:AAF7O2QwXt-56m81--lCahE7495mpL73adQ'
-    // Altre configurazioni possono essere aggiunte qui
-  };
-  
+  telegramBotToken: '6014587457:AAF7O2QwXt-56m81--lCahE7495mpL73adQ',
+  // Altre configurazioni possono essere aggiunte qui
+};
+
