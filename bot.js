9dfa0e7512ff239684b5b63e3a8a27584f437e0e
frankmano1 <109146593+frankmano1@users.noreply.github.com>
2023-09-10 03:52:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,119 +1,78 @@
 const TelegramBot = require('node-telegram-bot-api');
-const config = require('./config.js'); // Importa le tue configurazioni da config.js
+const mysql = require('mysql');
+const config = require('./config.js');
 
-const token = config.telegramBotToken; // Utilizza il token dal file config.js
-
+const token = config.telegramBotToken;
 const bot = new TelegramBot(token, { polling: true });
 
-// Opzioni del menu principale
-const mainMenuOptions = [
-  { text: 'Servizi', action: 'services' },
-  { text: 'Visita il sito', action: 'website' },
-  { text: 'Assistenza clienti', action: 'customerSupport' },
-  { text: 'Ricevi le nostre news', action: 'news' },
-  { text: 'Chatta con l\'AI', action: 'chatAI' },
-];
+const db = mysql.createConnection({
+  host: config.dbHost,
+  user: config.dbUser,
+  password: config.dbPassword,
+  database: config.dbName
+});
 
-// Azioni associate alle opzioni
-const optionActions = {
-  services: [
-    { text: 'Consulenza', action: 'webview', url: 'https://ititalia.it/consulenza' },
-    { text: 'Web App', action: 'webview', url: 'https://ititalia.it/web-app' },
-    { text: 'Formazione', action: 'webview', url: 'https://ititalia.it/formazione' },
-    { text: 'Portfolio', action: 'webview', url: 'https://portfolio.ititalia.it' },
-  ],
-  website: [{ text: 'Visita il sito', action: 'webview', url: 'https://ititalia.it' }],
-  customerSupport: [{ text: 'Assistenza clienti', action: 'webview', url: 'https://ititalia.it/assistenza' }],
-  news: [{ text: 'Ricevi le nostre news', action: 'action' }],
-  chatAI: [{ text: 'Chatta con l\'AI', action: 'action' }],
-};
-
-// Funzione per inviare un messaggio con opzioni
-function sendOptionsMessage(chatId, message, options) {
-  const optionsMessage = {
-    reply_markup: {
-      keyboard: options.map((option) => [option.text]),
-      resize_keyboard: true,
-      one_time_keyboard: true,
-    },
-  };
-  bot.sendMessage(chatId, message, optionsMessage);
-}
-
-// Funzione per gestire le azioni delle opzioni
-function handleOptionAction(chatId, action) {
-  if (optionActions[action]) {
-    // Azione associata valida
-    if (optionActions[action][0].action === 'webview') {
-      // Azione di tipo webview
-      const webviewOptions = {
-        reply_markup: {
-          inline_keyboard: [{ text: 'Apri', url: optionActions[action][0].url }],
-        },
-      };
-      bot.sendMessage(chatId, `Hai selezionato ${action}. Clicca sul pulsante per aprire la webview.`, webviewOptions);
-    } else if (optionActions[action][0].action === 'action') {
-      // Gestisci l'azione specifica qui
-      bot.sendMessage(chatId, `Hai selezionato ${action}.`);
-    }
+db.connect((err) => {
+  if (err) {
+    console.error('Errore nella connessione al database:', err);
   } else {
-    // Azione associata non valida
-    sendBackToMenuMessage(chatId);
+    console.log('Connessione al database MySQL avvenuta con successo!');
   }
-}
+});
 
-// Funzione per inviare il messaggio di ritorno al menu
-function sendBackToMenuMessage(chatId) {
-  const backToMenuMessage = 'Seleziona un\'opzione tra le seguenti così provo ad aiutarti:';
-  sendOptionsMessage(chatId, backToMenuMessage, mainMenuOptions);
-}
-
-// Funzione per gestire il messaggio di default
-function handleDefaultMessage(chatId, text) {
-  if (text === '/start') {
-    const welcomeMessage = 'Benvenuto su IT Italia! Sono il tuo assistente virtuale, non vedo l\'ora di aiutarti. Seleziona un\'opzione:';
-    sendOptionsMessage(chatId, welcomeMessage, mainMenuOptions);
-  } else if (text === '/help') {
-    const helpMessage = 'Sono qui per fornirti assistenza. Puoi scegliere un\'opzione dal menu oppure utilizzare i comandi seguenti:\n/start - Mostra il messaggio di benvenuto\n/help - Fornisce informazioni sul bot\n/info - Fornisce informazioni su IT Italia';
-    bot.sendMessage(chatId, helpMessage);
-  } else if (text === '/info') {
-    const botInfo = 'Sono un bot creato da IT Italia per fornire assistenza virtuale. Se hai bisogno di aiuto, non esitare a contattarmi.';
-    bot.sendMessage(chatId, botInfo);
-  } else {
-    bot.sendMessage(chatId, 'Mi dispiace, devi selezionare una delle opzioni indicate nei pulsanti o contattare un nostro agente');
-  }
-}
-
-// Gestione dei comandi personalizzati
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  handleDefaultMessage(chatId, '/start');
+  bot.sendMessage(chatId, 'Benvenuto su IT Italia, ti illustreremo tutte le funzionalità principali del nostro bot, ma prima presentiamoci!')
+    .then(() => {
+      setTimeout(() => {
+        bot.sendMessage(chatId, 'Come ti chiami? (Inserisci il tuo nome per continuare)');
+      }, 1000);
+    });
 });
 
-bot.onText(/\/help/, (msg) => {
-  const chatId = msg.chat.id;
-  handleDefaultMessage(chatId, '/help');
-});
-
-bot.onText(/\/info/, (msg) => {
-  const chatId = msg.chat.id;
-  handleDefaultMessage(chatId, '/info');
-});
-
-// Gestione dei messaggi di testo
-bot.on('message', (msg) => {
+bot.on('text', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  // Gestione dei comandi personalizzati
-  if (text.startsWith('/')) {
-    handleDefaultMessage(chatId, text);
-  } else {
-    // Gestione delle opzioni e azioni
-    mainMenuOptions.forEach((option) => {
-      if (text === option.text) {
-        handleOptionAction(chatId, option.action);
+  if (text.startsWith('Il mio nome è ')) {
+    const name = text.replace('Il mio nome è ', '');
+
+    // Salva il nome nel database
+    db.query('INSERT INTO utenti (chat_id, nome) VALUES (?, ?)', [chatId, name], (err, result) => {
+      if (err) {
+        console.error('Errore durante l\'inserimento nel database:', err);
+      } else {
+        // Invia un messaggio personalizzato con il nome utente
+        bot.sendMessage(chatId, `Piacere di conoscerti, ${name}! Ti chiediamo un'ultima informazione, potresti scriverci qui sotto il tuo indirizzo email?`);
       }
     });
+
+    // Crea una variabile con il nome utente
+    const username = name;
+
+    // Attendi una risposta per l'indirizzo email
+    bot.once('text', (msg) => {
+      const email = msg.text;
+
+      // Salva l'indirizzo email nel database
+      db.query('UPDATE utenti SET email = ? WHERE chat_id = ?', [email, chatId], (err, result) => {
+        if (err) {
+          console.error('Errore durante l\'aggiornamento nel database:', err);
+        } else {
+          bot.sendMessage(chatId, `Grazie per averci fornito il tuo indirizzo email, ${username}! Ora abbiamo tutte le informazioni necessarie.`);
+        }
+      });
+    });
+  } else {
+    bot.sendMessage(chatId, `Hai scritto: ${text}`);
   }
+});
+
+bot.on('polling_error', (error) => {
+  console.error(error);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Comando non riconosciuto. Prova /start per iniziare.');
 });
