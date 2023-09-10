6a0ef9042351a097d17221c9581cd5837d56cbb3
frankmano1 <109146593+frankmano1@users.noreply.github.com>
2023-09-10 03:51:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,10 @@
 
 module.exports = {
     telegramBotToken: '6014587457:AAF7O2QwXt-56m81--lCahE7495mpL73adQ',
+    dbHost: '31.11.39.94',
+    dbUser: 'Sql1656620',
+    dbPassword: '!3*qHv*oAI!2k7g0',
+    dbName: 'Sql1656620_3'
     // Altre configurazioni possono essere aggiunte qui
   };
   
