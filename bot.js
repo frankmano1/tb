f2a88fdb0e40dd6e26cf2477827b3549857f1ffa
frankmano1 <109146593+frankmano1@users.noreply.github.com>
2023-09-11 03:13:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,128 +3,171 @@ const config = require('./config'); // Importa il file di configurazione
 
 const bot = new TelegramBot(config.telegramBotToken, { polling: true });
 
-// Messaggio di benvenuto
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  const welcomeMessage = "Benvenuto su IT Italia! Seleziona il menu che desideri cliccando su uno dei pulsanti disponibili.";
-  const keyboard = {
+// Funzione per inviare un messaggio con opzioni
+function sendOptionsMessage(chatId, message, options) {
+  const optionsMessage = {
     reply_markup: {
-      keyboard: [
-        [
-          { text: 'Chi siamo' },
-          { text: 'Consulenza' },
-          { text: 'Siti web' },
-          { text: 'Formazione' },
-          { text: 'Portfolio' },
-        ],
-        [
-          { text: 'Le nostre news' },
-          { text: 'Contatti' },
-          { text: 'Assistenza Clienti' },
-          { text: 'Seguici sui Social' },
-        ],
-      ],
-    },
+      keyboard: options,
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
   };
-
-  bot.sendMessage(chatId, welcomeMessage, keyboard);
-});
-
-// Gestione dei pulsanti del menu
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
-
-  switch (text) {
-    case 'Chi siamo':
-      bot.sendMessage(chatId, 'Puoi trovare informazioni su di noi su https://ititalia.it');
-      break;
-    case 'Consulenza':
-      bot.sendMessage(chatId, 'Per la consulenza, visita https://ititalia.it/consulenza');
-      break;
-    case 'Siti web':
-      bot.sendMessage(chatId, 'Scopri i nostri siti web su https://ititalia.it/siti-web');
-      break;
-    case 'Formazione':
-      bot.sendMessage(chatId, 'Per la formazione, visita https://ititalia.it/formazione');
-      break;
-    case 'Portfolio':
-      bot.sendMessage(chatId, 'Esplora il nostro portfolio su https://portfolio.ititalia.it');
-      break;
-    case 'Le nostre news':
-      bot.sendMessage(chatId, 'Leggi le nostre news su https://ititalia.it/blog');
-      break;
-    case 'Contatti':
-      bot.sendMessage(chatId, 'Contattaci su https://ititalia.it/contatti');
-      break;
-    case 'Assistenza Clienti':
-      bot.sendMessage(chatId, 'Per assistenza clienti, vai su https://ititalia.it/assistenza');
-      break;
-    case 'Seguici sui Social':
-      // Creare il menu dei social come descritto successivamente
-      sendSocialMenu(chatId);
-      break;
-    default:
-      bot.sendMessage(chatId, 'Scelta non valida. Seleziona un'opzione dal menu.');
-  }
-});
-
-// Funzione per inviare il menu dei social
-function sendSocialMenu(chatId) {
-  const socialKeyboard = {
-    reply_markup: {
-      keyboard: [
-        [
-          { text: 'Facebook' },
-          { text: 'Twitter' },
-          { text: 'Instagram' },
-          { text: 'LinkedIn' },
-        ],
-        [
-          { text: 'YouTube' },
-          { text: 'Telegram' },
-          { text: 'Torna al menu principale' },
-        ],
-      ],
-    },
-  };
-
-  bot.sendMessage(chatId, 'Seguici sui seguenti social media:', socialKeyboard);
+  bot.sendMessage(chatId, message, optionsMessage);
 }
 
-// Gestione dei pulsanti dei social
+// Funzione per gestire le azioni delle opzioni
+function handleOptionAction(chatId, action) {
+  const backToOptions = {
+    reply_markup: {
+      keyboard: [
+        ['Su di noi'],
+        ['Opzione 2'],
+        ['Opzione 3'],
+        ['Opzione 4'],
+        ['Opzione 5']
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  };
+
+  if (action === 'I nostri servizi') {
+    // Apri la webview con l'URL specificato
+    const webviewOptions = {
+      reply_markup: {
+        inline_keyboard: [[{ text: 'Apri', url: 'https://ititalia.it/servizi' }]]
+      }
+    };
+    bot.sendMessage(chatId, 'Hai selezionato I nostri servizi. Clicca sul pulsante per aprire la webview.', webviewOptions);
+  } else if (action === 'Portfolio') {
+    // Apri la webview con l'URL specificato
+    const webviewOptions = {
+      reply_markup: {
+        inline_keyboard: [[{ text: 'Apri', url: 'https://portfolio.ititalia.it' }]]
+      }
+    };
+    bot.sendMessage(chatId, 'Hai selezionato Portfolio. Clicca sul pulsante per aprire la webview.', webviewOptions);
+  } else {
+    bot.sendMessage(chatId, 'Hai selezionato ' + action, backToOptions);
+  }
+}
+
+// Funzione per inviare il messaggio di ritorno al menu
+function sendBackToMenuMessage(chatId) {
+  const backToMenuMessage = 'Seleziona un\'opzione tra le seguenti così provo ad aiutarti:';
+
+  const menuOptions = {
+    reply_markup: {
+      keyboard: [
+        ['Su di noi'],
+        ['Opzione 2'],
+        ['Opzione 3'],
+        ['Opzione 4'],
+        ['Opzione 5']
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  };
+
+  bot.sendMessage(chatId, backToMenuMessage, menuOptions);
+}
+
+// Funzione per gestire il messaggio di default
+function handleDefaultMessage(chatId) {
+  bot.sendMessage(chatId, 'Mi dispiace, devi selezionare una delle opzioni indicate nei pulsanti o contattare un nostro agente');
+}
+
+// Gestione dei messaggi
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  switch (text) {
-    case 'Facebook':
-      bot.sendMessage(chatId, 'Seguici su Facebook su [link_facebook]');
-      break;
-    case 'Twitter':
-      bot.sendMessage(chatId, 'Seguici su Twitter su [link_twitter]');
-      break;
-    case 'Instagram':
-      bot.sendMessage(chatId, 'Seguici su Instagram su [link_instagram]');
-      break;
-    case 'LinkedIn':
-      bot.sendMessage(chatId, 'Seguici su LinkedIn su [link_linkedin]');
-      break;
-    case 'YouTube':
-      bot.sendMessage(chatId, 'Seguici su YouTube su [link_youtube]');
-      break;
-    case 'Telegram':
-      bot.sendMessage(chatId, 'Unisciti al nostro canale Telegram su [link_telegram]');
-      break;
-    case 'Torna al menu principale':
-      // Torna al menu principale
-      bot.sendMessage(chatId, "Torna al menu principale.", {
-        reply_markup: {
-          remove_keyboard: true,
-        },
-      });
-      break;
-    default:
-      bot.sendMessage(chatId, 'Scelta non valida. Seleziona un'opzione dal menu.');
+  // Controllo se il messaggio è un comando personalizzato
+  if (text.startsWith('/')) {
+    // Gestione dei comandi personalizzati
+    switch (text) {
+      case '/start':
+        const welcomeMessage = 'Benvenuto su IT Italia! Sono il tuo assistente virtuale, non vedo l\'ora di aiutarti. Seleziona un\'opzione tra le seguenti:';
+        const startOptions = [
+          ['Su di noi'],
+          ['Opzione 2'],
+          ['Opzione 3'],
+          ['Opzione 4'],
+          ['Opzione 5']
+        ];
+        sendOptionsMessage(chatId, welcomeMessage, startOptions);
+        break;
+
+      case '/help':
+        const helpMessage = 'Sono qui per fornirti assistenza. Puoi scegliere un\'opzione dal menu oppure utilizzare i comandi seguenti:\n/start - Mostra il messaggio di benvenuto\n/help - Fornisce informazioni sul bot\n/info - Fornisce informazioni su IT Italia';
+        bot.sendMessage(chatId, helpMessage);
+        break;
+
+      case '/info':
+        const botInfo = 'Sono un bot creato da IT Italia per fornire assistenza virtuale. Se hai bisogno di aiuto, non esitare a contattarmi.';
+        bot.sendMessage(chatId, botInfo);
+        break;
+
+      default:
+        // Messaggio di default per comandi non riconosciuti
+        bot.sendMessage(chatId, 'Comando non valido');
+        break;
+    }
+  } else {
+    // Gestione delle opzioni e azioni
+    switch (text) {
+      case 'Su di noi':
+        const aboutUsMessage = 'Hai selezionato Su di noi. Scegli un\'azione:';
+        const aboutUsActions = [['I nostri servizi'], ['Portfolio'], ['Azione 2'], ['Torna al menu']];
+        sendOptionsMessage(chatId, aboutUsMessage, aboutUsActions);
+        break;
+
+      case 'Opzione 2':
+        const option2Message = 'Hai selezionato Opzione 2. Scegli un\'azione:';
+        const option2Actions = [['Azione 3'], ['Azione 4'], ['Torna al menu']];
+        sendOptionsMessage(chatId, option2Message, option2Actions);
+        break;
+
+      case 'Opzione 3':
+        const option3Message = 'Hai selezionato Opzione 3. Scegli un\'azione:';
+        const option3Actions = [['Azione 5'], ['Azione 6'], ['Torna al menu']];
+        sendOptionsMessage(chatId, option3Message, option3Actions);
+        break;
+
+      case 'Opzione 4':
+        const option4Message = 'Hai selezionato Opzione 4. Scegli un\'azione:';
+        const option4Actions = [['Azione 7'], ['Azione 8'], ['Torna al menu']];
+        sendOptionsMessage(chatId, option4Message, option4Actions);
+        break;
+
+      case 'Opzione 5':
+        const option5Message = 'Hai selezionato Opzione 5. Scegli un\'azione:';
+        const option5Actions = [['Azione 9'], ['Azione 10'], ['Torna al menu']];
+        sendOptionsMessage(chatId, option5Message, option5Actions);
+        break;
+
+      case 'I nostri servizi':
+      case 'Portfolio':
+      case 'Azione 2':
+      case 'Azione 3':
+      case 'Azione 4':
+      case 'Azione 5':
+      case 'Azione 6':
+      case 'Azione 7':
+      case 'Azione 8':
+      case 'Azione 9':
+      case 'Azione 10':
+        handleOptionAction(chatId, text);
+        break;
+
+      case 'Torna al menu':
+        sendBackToMenuMessage(chatId);
+        break;
+
+      default:
+        handleDefaultMessage(chatId);
+        break;
+    }
   }
 });
